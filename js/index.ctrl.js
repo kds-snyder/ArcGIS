02c696e7204e2afec8a61bf829eab8e91152c3cf
Karen Snyder <karen.d.snyder@advantagesolutions.net>
2019-09-12 14:35:50 -0700

    Add layer from hosted feature set

--- js/index.ctrl.js
+++ js/index.ctrl.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
+    require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function (Map, MapView, FeatureLayer) {
         var map = new Map({
             basemap: "streets"
         });
@@ -11,6 +11,11 @@ $(document).ready(function () {
             zoom: 5, // Sets zoom level based on level of detail (LOD)
             center: [-97.6114, 38.8403] // Sets center point of view (Salinas, Kansas) using longitude,latitude
         });
+
+        var officeLocationsLayer = new FeatureLayer({
+            url: "https://services6.arcgis.com/rRYwal15YiDfaDTK/arcgis/rest/services/advantage_offices/FeatureServer/0"
+        });
+        map.add(officeLocationsLayer);
     });
 
 });
